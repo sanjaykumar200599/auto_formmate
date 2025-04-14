25effc88d63f5f737291dc6ebd75d51395b3254a
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-04-14 12:01:03 +0530

    prisma is updated

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20">
     <Button>Please subscribe</Button>
+    <h1>Helo world</h1>
     </div>
   );
 }
