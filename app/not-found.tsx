4f2d83f23404e229cc40f702874ff799fb73457e
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-04-18 12:19:06 +0530

    Not founf page updated

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+    <div className="max-w-md text-center">
+      <h1 className="text-5xl font-bold text-red-600 mb-4">Error</h1>
+      <h2 className="text-2xl font-semibold text-gray-800 mb-2">Page Not Found ⚠️</h2>
+      <p className="text-gray-600 mb-6">
+        The page you're looking for doesn't exist or has been moved.
+      </p>
+      <a
+        href="/"
+        className="inline-block px-6 py-3 bg-red-600 text-white rounded-2xl shadow hover:bg-green-700 transition"
+      >
+        Go Home
+      </a>
+    </div>
+  </div>
+  )
+}
+
+export default PageNotFound
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20">
     <Button>Please subscribe</Button>
-    <h1>Helo world</h1>
     </div>
   );
 }
