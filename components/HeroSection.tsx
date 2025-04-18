920e0f9ba12ce0a97957edca82788666f62034c4
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-04-18 17:38:05 +0530

    preparing user interface

---
+++ components/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className='text-center '>
+            <h1 className='text-4xl font-bold'>Build AI-Drivern Forms Effortlessly</h1>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ components/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+        <h1 className='font-extrabold text-2xl'>Formify.ai</h1>
+    </div>
+  )
+}
+
+export default Logo
